77d29f1eb9b7dc4b0b099e26abdaca887def714a
Julio Pérez <distruccion.dev@gmail.com>
2022-07-22 00:40:42 -0600

    primer commit

---
+++ img/resize_img.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import os
+
+# os.chdir("D:/Distrucción/Web/Page/public/img")
+ 
+print("Shrink images in the folder")
+folder = input("folder: ")
+w = int(input("width: "))
+h = int(input("height: "))
+for i in os.listdir(folder):
+    file = f"{folder}\\{i}"
+    im = Image.open(file)
+    im = im.resize((w, h), Image.ANTIALIAS)
+    im.save(file)
---
+++ js/dist_script.js
@@ -0,0 +1,25 @@
+//Cambiar color icono messenger
+
+function onHover_messenger() {
+    document.getElementById("messenger").src = "assets/messenger_hover.svg";
+}
+function offHover_messenger() {
+    document.getElementById("messenger").src = "assets/messenger.svg";
+}
+
+//Cambiar color arrows_silider
+
+function onHover_arrowL() {
+    document.getElementById("before").src = "assets/arrowL_h.svg";
+}
+function offHover_arrowL() {
+    document.getElementById("before").src = "assets/arrowL.svg";
+}
+
+function onHover_arrowR() {
+    document.getElementById("next").src = "assets/arrowR_h.svg";
+}
+function offHover_arrowR() {
+    document.getElementById("next").src = "assets/arrowR.svg";
+}
+
